f3d4fa54daeedbf7dc4e44d895c1a189122781ba
Pablo <pablohenriqueop@gmail.com>
2022-06-10 18:03:32 -0300

    changed socialIcons on footer and add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import "../styles/globals.css";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "G-ETG9WP4T77",
+};
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: tagManagerArgs.gtmId });
+  }, []);
   return (
     <>
       <Head>
